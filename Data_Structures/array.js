3d9a831b1225684f361d319d2437c7adefbf741e
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-13 14:15:10 +0530

    Problem 2 of array

--- Data_Structures/array.js
+++ Data_Structures/array.js
@@ -7,3 +7,16 @@
  }
  let result = sum/marks.length;
  console.log(result);
+
+ /* Practice problem 2
+   For a given array [250,645,300,900,50] find 10% of 
+   the elements and update the array*/
+ let values =   [250,645,300,900,50];
+ let offer = 0;
+ for(let i=0;i<values.length;i++){
+  offer = values[i]/10;
+  values[i] = values[i] - offer;
+  
+ }
+
+console.log(values)
