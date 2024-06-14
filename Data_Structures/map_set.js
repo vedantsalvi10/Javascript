51f30ee958787ee09028e209112b664cc514e3ed
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-14 14:25:28 +0530

    complete of map and sets

--- Data_Structures/map_set.js
+++ Data_Structures/map_set.js
@@ -2,9 +2,35 @@
  let map = new Map([["uber","$4324"]]); 
  map.set(["ola","$2131"],["blinkit", "$23424"]);
  console.log(map);
+ map.forEach(element => {
+   console.log(element);
+ });
 
- /* Practicing set using comapies as keys and thier price as value*/
+ /* Practicing set using numbers*/
  let arr = [1,2,3,4]
  let set = new Set(arr); 
  set.add(5)
+ set.add(5)
  console.log(set);
+ set.forEach(element => {
+    console.log(element);
+ });
+ set.delete(2);
+ console.log(set);
+
+/* Practicing weak set */
+ obj1 = {name:"vedant"};
+ obj2 = null;
+ let weakset =new WeakSet();
+ weakset.add(obj1);
+ //weakset.add(obj2); // shows that the object is annonymus.
+ console.log(weakset);
+
+ 
+ /* Practicing weak map */
+ let weakmap = new WeakMap()
+ weakmap.set(obj1,"vedant");
+//  weakmap.set(obj2,"ram"); // asumes it is a garbage value
+ //weakset.set("2","sha"); // only accepts objects.
+ console.log(weakmap.get(obj1));
+ console.log(weakmap);
