d67437756bf79093d13e87235dcf0abb370b9bea
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-14 14:23:15 +0530

    examples of map, set, weakmap, weakset

---
+++ Data_Structures/map_set.js
@@ -0,0 +1,10 @@
+/* Practicing mapping using comapies as keys and thier price as value*/
+ let map = new Map([["uber","$4324"]]); 
+ map.set(["ola","$2131"],["blinkit", "$23424"]);
+ console.log(map);
+
+ /* Practicing set using comapies as keys and thier price as value*/
+ let arr = [1,2,3,4]
+ let set = new Set(arr); 
+ set.add(5)
+ console.log(set);
