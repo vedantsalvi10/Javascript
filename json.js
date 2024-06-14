9754e510874ec599a97724ef0c191e042cce8347
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-14 16:14:35 +0530

    trying different properties for json

--- json.js
+++ json.js
@@ -1,23 +1,28 @@
 let members = `[
-                //member 1
-                {
-                 "name": "Vedant",
-                 "age":34
-                 "duration":"1 year"
-                 "hobies":["driving","swiming","dancing","drawing"]},
-                 
-                //  member 2
-                 {
-                 "name": "Gacumoli",
-                 "age":25
-                 "duration":"2 year"
-                 "hobies":["farming","swiming","dancing","football"]},
-                
-                // member 3
-                 {
-                 "name": "chirag",
-                 "age":40
-                 "duration":"5 year"
-                 "hobies":["criket","browsing","dancing","chess"]}
-                 }
-                 ]`
+
+{
+ "name": "Vedant",
+ "age":34,
+ "duration":"1 year",
+ "hobies":["driving","swiming","dancing","drawing"]
+ },
+ 
+
+ {
+ "name": "Gacumoli",
+ "age":25,
+ "duration":"2 year",
+ "hobies":["farming","swiming","dancing","football"]
+ },
+
+
+ {
+ "name": "chirag",
+ "age":40,
+ "duration":"5 year",
+ "hobies":["criket","browsing","dancing","chess"]
+ }
+ ]`
+ console.log(JSON.parse(members));
+ console.log(JSON.parse(members)[1].name);
+ console.log(JSON.parse(members)[2].hobies[3]);
