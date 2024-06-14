de8bc9fae66c492ef982651743727f1ea0eb33fd
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-14 15:53:54 +0530

    adding members in the javascript

---
+++ json.js
@@ -0,0 +1,23 @@
+let members = `[
+                //member 1
+                {
+                 "name": "Vedant",
+                 "age":34
+                 "duration":"1 year"
+                 "hobies":["driving","swiming","dancing","drawing"]},
+                 
+                //  member 2
+                 {
+                 "name": "Gacumoli",
+                 "age":25
+                 "duration":"2 year"
+                 "hobies":["farming","swiming","dancing","football"]},
+                
+                // member 3
+                 {
+                 "name": "chirag",
+                 "age":40
+                 "duration":"5 year"
+                 "hobies":["criket","browsing","dancing","chess"]}
+                 }
+                 ]`
