f654fa5f369bb087076111d4ccc73a36e929c741
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-15 17:32:59 +0530

    problem 2

--- conditional_statements/c_statements.js
+++ conditional_statements/c_statements.js
@@ -1,7 +1,26 @@
+// practice problem 1
 let n = prompt("enter a number: ");
 if(n% 5 === 0){
   alert("divisible by 5");
 }
 else{
   alert("not divisible by 5");
+}
+
+//practice problem 2
+let score = prompt("enter ur score: ");
+if(score >=80   && score<=100){
+    alert("congrats, A");
+}
+else if(score >=70   && score<=79){
+  alert("congrats, B");
+}
+else if(score >=60   && score<=69){
+  alert("congrats, C");
+}
+else if(score >=50   && score<=59){
+  alert("congrats, D");
+}
+else {
+  alert("fail, f");
 }
