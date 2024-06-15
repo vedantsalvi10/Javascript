d33f67dc137f2c48ad604eda51950f1361579ee1
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-15 13:24:09 +0530

    game

--- loops/loops.js
+++ loops/loops.js
@@ -4,3 +4,12 @@ for(let i =0;i<101;i++){
   }
   
 }
+
+// number game (Ask user to guess the number)
+
+let n = 3;
+let user = prompt("guess a number from 1-20");
+while ( n !== user){
+  user = prompt("guess a number from 1-20");
+}
+ alter("congrats");
