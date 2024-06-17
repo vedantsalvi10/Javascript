3811717d8d7686cd84537d98f3bbc87781a4dc1a
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-17 17:26:47 +0530

    learning funnctions.

---
+++ functions/functions.js
@@ -0,0 +1,10 @@
+function counter(string){
+  for(character in string){
+    if(character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u'){
+      console.log("string has vowel.");
+    }
+    else{
+      console.log("string has no vowel.");
+    }
+  }
+}
