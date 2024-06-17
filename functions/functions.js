d97dcbb327bc7319f473843e1c086d53f936f391
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-17 17:32:19 +0530

    adding a arrow function

--- functions/functions.js
+++ functions/functions.js
@@ -9,3 +9,14 @@ function counter(string){
     return n;
 
 }
+
+// arrow function to perform same task 
+const count = (string) =>{
+  let n = 0;
+  for(character of string){
+    if(character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u'){
+      n++
+    }
+    return n
+  }
+}
