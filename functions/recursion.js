3e3128398ed0dd8d4ee344ebb920cb4b5cc9f061
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-18 14:22:54 +0530

    doing factorial using recursion

--- functions/recursion.js
+++ functions/recursion.js
@@ -7,3 +7,14 @@ const rec = (n)=>{
   return n + rec(n-1);
  }
 }
+
+// factorial using recurssion
+function fac(n){
+  if(n==1 || n==0){
+    return 1;
+  }
+  else{
+    return n * fac(n-1)
+  }
+};
+
