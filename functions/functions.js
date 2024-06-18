fc9ffe7a2f8c380d42c88405e9986e2cc67ebee5
vedantsalvi10 <vedantsalvi10@gmail.com>
2024-06-18 13:59:56 +0530

    learning more abt functions

--- functions/functions.js
+++ functions/functions.js
@@ -1,11 +1,14 @@
 // find vowel in the string....
-function counter(string){
+function counter( s = "vedant"){
   let n = 0;
-  for(character of string){
+  for(character of s){
     if(character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u'){
       n++
     }
   }
+   // argument in functions
+   console.log(arguments[0]);
+
     return n;
 
 }
@@ -20,3 +23,33 @@ const count = (string) =>{
     return n
   }
 }
+
+// iife immediatly invoked functions
+(function vedant(string){
+  console.log(string)
+})("vedant")
+
+ 
+
+// few array methods using function
+
+//map
+let arr = ["vedant","ram"," sham"," hans"];
+let name = arr.map((val)=>{
+  return val
+});
+console.log(name);
+
+//filter
+let nam = [];
+let filter = arr.filter((val)=>{
+  return val === "vedant" ? console.log("false"):nam += val;
+});
+console.log(nam);
+
+//reduce
+let reduce = arr.reduce((val)=>{
+  return val + val;
+})
+console.log(reduce)
+
